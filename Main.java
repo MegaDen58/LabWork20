6d67dac2ca9b8be303db6fcaabcde575ae8fbe2c
Denis <samparizona2018@yandex.ru>
2022-05-28 08:17:40 +0300

    dd

--- Main.java
+++ Main.java
@@ -1,98 +1,67 @@
 package com.company;
 
-import java.util.Scanner;
+interface HighConnect{
+    public void Charge();
+    public void HighPower();
+}
 
+interface LowConnect{
+    public void Charge();
+    public void LowPower();
+}
 
-class Complex {
-    private double x1 = 0;
-    private double x2 = 0;
-    private double i1 = 0;
-    private double i2 = 0;
-    private static Complex complex = new Complex();
-
-
-    public String complexSum() {
-        double first = x1 + x2;
-        double second = i1 + i2;
-        if (second == 0) {
-            return first + " + " + "i";
-        }
-        else if(first == 0){
-            return second + "i";
-        }
-        else{
-            return first + " + " + second + "i";
-        }
+class HighVolt implements HighConnect{
+    public void Charge(){
+        System.out.println("Телефон заряжается.");
     }
-
-    public String complexSub() {
-        double first = x1 - x2;
-        double second = i1 - i2;
-        if(second > 0){
-            return first + " + " + "i";
-        }
-        if(second < 0){
-            return first + " - " + second * -1 + "i";
-        }
-        if (second == 0 | x2 == 0) {
-            return first + " - " + "i";
-        }
-        else if(first == 0){
-            return second + "i";
-        }
-        else{
-            return first + " - " + second + "i";
-        }
+    public void HighPower(){
+        System.out.println("Мощность розетки 380 Вольт.");
     }
+}
 
-
-    public String complexMulti(){
-        double first = x1 * x2 - i1 * i2;
-        double second = i1 * x2 + x1 * i2;
-        if(second == 1){
-            return first + " + "+ "i";
-        }
-        else{
-            return first + " + " + second + "i";
-        }
+class LowVolt implements LowConnect{
+    public void Charge(){
+        System.out.println("Телефон заряжается.");
     }
-
-    public String complexDiv(){
-        double first = ((x1 * x2 + i1 * i2)) / (Math.pow(x2, 2) + Math.pow(i2, 2));
-        double second = ((i1 * x2 - x1 * i2)) / (Math.pow(x2, 2) + Math.pow(i2, 2));
-        return first + " + " + second + "i";
+    public void LowPower(){
+        System.out.println("Мощность розетки 220 Вольт.");
     }
+}
 
-
-    public static Complex getInstance() {
-        return complex;
+class Socket{
+    private HighConnect highConnect;
+    public Socket(HighConnect highConnect){
+        this.highConnect = highConnect;
     }
+    public void work(){
+        highConnect.Charge();
+        highConnect.HighPower();
+    }
+}
 
-    public void setParameters(double x1, double x2, double i1, double i2) {
-        this.x1 = x1;
-        this.x2 = x2;
-        this.i1 = i1;
-        this.i2 = i2;
+class SocketAdapter implements HighConnect{
+    LowConnect lowConnect;
+    public SocketAdapter(LowConnect lowConnect){
+        this.lowConnect = lowConnect;
+    }
+    public void Charge(){
+        lowConnect.Charge();
+    }
+    public void HighPower(){
+        lowConnect.LowPower();
     }
 }
 
 
+
+
     public class Main {
         public static void main(String[] args) {
-            Scanner in = new Scanner(System.in);
-            System.out.print("Введите первую действительную часть: ");
-            double x1 = in.nextDouble();
-            System.out.print("Введите первую мнимую единицу: ");
-            double i1 = in.nextDouble();
-            System.out.print("Введите вторую действительную часть: ");
-            double x2 = in.nextDouble();
-            System.out.print("Введите вторую мнимую единицу: ");
-            double i2 = in.nextDouble();
-            Complex comp = Complex.getInstance();
-            comp.setParameters(x1, x2, i1, i2);
-            System.out.printf("Сумма = %s\n", comp.complexSum());
-            System.out.printf("Разность = %s\n", comp.complexSub());
-            System.out.printf("Произведение = %s\n", comp.complexMulti());
-            System.out.printf("Частное = %s\n",comp.complexDiv());
+            HighVolt highVolt = new HighVolt();
+            Socket highSocket = new Socket(highVolt);
+            highSocket.work();
+            SocketAdapter socketAdapter = new SocketAdapter(new LowVolt());
+            Socket lowSocket = new Socket(socketAdapter);
+            lowSocket.work();
         }
     }
